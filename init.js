77800779a4f3833c76846b19f1328140e193dd16
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2021-11-09 17:35:38 +0530

    navigation contents

--- init.js
+++ init.js
@@ -1,9 +1,22 @@
-let navbar = document.getElementById("nav");
-navbar.className = "navbar" ;
-let navigation = document.createElement("div");
-navigation.innerHTML = "solutions";
-navigation.className = "solutioncontents";
-navbar.append(navigation);
+// let navbar = document.getElementById("nav");
+// navbar.className = "navbar" ;
+// let navigation = document.createElement("div");
+// navigation.innerHTML = "solutions";
+// navigation.className = "solutioncontents";
+// navbar.append(navigation);
 
+// // creating dropbox
+// let dropbox = document.createElement("div");
+// dropbox.className = "dropbox";
+// navigation.append(dropbox);
+
+// // dropbox contents
+// let contents = document.createElement("ul");
+// contents.id = "contents";
+// dropbox.append(contents);
+
+// let contentHome = document.createElement("li");
+// contentHome.innerHTML = "Home";
+// contents.append(contentHome);
 
 
