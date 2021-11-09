cad63cc64022cf32cfdeb498838528fed1be8751
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2021-11-09 14:22:42 +0530

    navigationbar contents

--- init.js
+++ init.js
@@ -1,3 +1,9 @@
-let navbar = document.createElement("div");
-navbar.innerHTML = "home" ;
+let navbar = document.getElementById("nav");
+navbar.className = "navbar" ;
+let navigation = document.createElement("div");
+navigation.innerHTML = "solutions";
+navigation.className = "solutioncontents";
+navbar.append(navigation);
+
+
 
